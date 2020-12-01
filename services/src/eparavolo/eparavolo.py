06468e00c41b6c68c3fd559613898717e92d06ab
Chris Karageorgiou Kaneen <ckarageorgkaneen@gmail.com>
2020-12-01 15:06:41 +0200

    Add error handling in eparavolo

--- services/src/eparavolo/eparavolo.py
+++ services/src/eparavolo/eparavolo.py
@@ -1,12 +1,37 @@
-#!/usr/bin/env python3
 import datetime
+import functools
+import logging
+import requests
 import yaml
 import zeep
 
 from zeep.wsse.username import UsernameToken
 
+from zeep.exceptions import Error as ZeepError
+
 from .error import eParavoloErrorCode as ErrorCode
 from services.data import IN_FILES
+
+logger = logging.getLogger(__name__)
+
+
+class eParavoloError(Exception):
+    """eParavoloError exception"""
+
+
+def _error(message):
+    logger.error(message)
+    raise eParavoloError(message)
+
+
+def _handle_zeep_error(f):
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        try:
+            return f(*args, **kwargs)
+        except (ZeepError, requests.exceptions.HTTPError) as e:
+            _error(str(e))
+    return wrapper
 
 
 def _request_data(code):
@@ -28,7 +53,7 @@ def _request_data(code):
 
 class eParavolo:
 
-    WSDL_URL = 'https://test.gsis.gr/esbpilot/eparavoloPublicService?wsdl'
+    _WSDL_URL = 'https://test.gsis.gr/esbpilot/eparavoloPublicService?wsdl'
 
     def __init__(self):
         self.__client = None
@@ -38,20 +63,29 @@ class eParavolo:
         if self.__client is None:
             with open(IN_FILES['eparavolo_credentials']) as file:
                 credentials = yaml.load(file, Loader=yaml.FullLoader)
-            self.__client = zeep.Client(wsdl=self.WSDL_URL,
-                                        wsse=UsernameToken(
-                                            credentials['username'],
-                                            credentials['password']))
+            self.__client = self._zeep_client(credentials['username'],
+                                              credentials['password'])
         return self.__client
 
+    @_handle_zeep_error
+    def _zeep_client(self, username, password):
+        return zeep.Client(wsdl=self._WSDL_URL,
+                           wsse=UsernameToken(
+                               username,
+                               password))
+
+    @_handle_zeep_error
+    def _type_info_output_record(self, code):
+        return self._client.service.getParavoloTypeInfo(
+            **_request_data(code)).getParavoloTypeInfoOutputRecord
+
     def fetch(self, code):
-        response_obj = self._client.service.getParavoloTypeInfo(
-            **_request_data(code))
-        output_record_obj = response_obj.getParavoloTypeInfoOutputRecord
-        if output_record_obj is None:
+        type_info_output_record = self._type_info_output_record(code)
+        if type_info_output_record is None:
             result = ErrorCode.NOT_FOUND
         else:
-            response_dict = zeep.helpers.serialize_object(output_record_obj)
+            response_dict = zeep.helpers.serialize_object(
+                type_info_output_record)
             response_dict['price'] = int(response_dict['price'])
             result = response_dict
         return result
