7f02fc8913c2524de9bce78e514035a4e5eb4b1d
Chris Karageorgiou Kaneen <ckarageorgkaneen@gmail.com>
2020-12-14 15:37:28 +0200

    Add script for deleting broken org pages

---
+++ scripts/delete_broken_org_pages.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from services.src.site import Site
+
+site = Site()
+site.auto_login()
+
+for page in site.categories['Κατάλογος Φορέων']:
+    page_title = page.page_title
+    if page_title.startswith('Broken/'):
+        page.delete()
+        print(f'{page_title} deleted.')
