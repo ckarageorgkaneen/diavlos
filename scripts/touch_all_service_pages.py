534531780a539d19abc07746f22c5ce90abf59f0
Chris Karageorgiou Kaneen <ckarageorgkaneen@gmail.com>
2021-02-18 13:10:54 +0200

    Add script to touch (null-edit) all service pages

---
+++ scripts/touch_all_service_pages.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from diavlos.src.site import Site
+site = Site()
+site.auto_login()
+process_namespaces = [
+    '9002',  # ΔΔ
+    '9006',  # ΥΕ
+    '9008',  # ΠΕ
+    '9010',  # ΠΔ
+]
+for ns in process_namespaces:
+    for page in site._client.allpages(namespace=ns):
+        page.touch()
+        print(f'{page.page_title} touched.')
