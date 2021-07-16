7f450897b07fe58439f29e00549d4834f6b5b7e2
Nikos Pappas <npappas@admin.grnet.gr>
2021-07-16 16:03:17 +0300

    ada validation code refactor vol.3

--- diavlos/src/diaugeia/error.py
+++ diavlos/src/diaugeia/error.py
@@ -7,7 +7,6 @@ class DiaugeiaErrorCode(ErrorCode):
 
 
 DiaugeiaErrorData = ErrorData()
-print(DiaugeiaErrorCode.NOT_FOUND)
 DiaugeiaErrorData.add(DiaugeiaErrorCode.NOT_FOUND,
                       'Δεν βρέθηκε ο αριθμός διαδικτυακής ανάρτησης',
                       200)
